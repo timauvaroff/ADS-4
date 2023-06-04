31afe7f11ae49c84160144614e81edf43743c6e7
Уваров Тимофей Максимович <66253074+timauvaroff@users.noreply.github.com>
2023-06-04 15:05:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,55 +1,54 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-      int count = 0;
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value)
-                count++;
-            else if (arr[i] + arr[j] > value)
-                break;
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+      for (int j = i + 1; j < len; ++j) {
+        if (arr[i] + arr[j] == value) {
+          count++;
         }
+      }
     }
-    return count;
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-    int left = 0;
-    int right = len - 1;
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            count++;
-            left++;
-            right--;
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
-        }
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = len - 1; j > i; j--) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
     }
-    
-    return count;
-}
-int binSearch(int *arr, int low, int high, int value) {
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] == value)
-            return mid;
-        else if (arr[mid] < value)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    
-    return -1;
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
-        int secondElement = value - arr[i];
-        int index = binSearch(arr, i + 1, len - 1, secondElement);
-        if (index!=-1)
-          count++;
+        int right = len;
+        int left = i;
+        while (left < right - 1) {
+            int mid = (left + right) / 2;
+            if (arr[i] + arr[mid] == value) {
+                count++;
+                int confmid = mid + 1;
+                while (arr[i] + arr[confmid] == value && confmid < right) {
+                    count++;
+                    confmid++;
+                }
+                confmid = mid - 1;
+                while (arr[i] + arr[confmid] == value && confmid > left) {
+                    count++;
+                    confmid--;
+                }
+                break;
+            }
+            if (arr[i] + arr[mid] > value) {
+                right = mid;
+            }
+            else {
+                left = mid;
+            }
+        }
     }
-  return count;
+    return count;
 }
